9a742876cd7366b334450b4d167a51e1ae1e4dd3
emisuke69 <emilioyusuke@gmail.com>
2026-07-04 01:10:18 +0200

    revert: roll back all mobile-bar/detection work to pre-mobile baseline (fd01b2c)

--- sw.js
+++ sw.js
@@ -24,19 +24,7 @@ self.addEventListener('activate', e => {
 });
 
 self.addEventListener('fetch', e => {
-  const url = new URL(e.request.url);
-  const isHTML = url.pathname.endsWith('.html') || url.pathname === '/' || url.pathname.endsWith('/');
-  if (isHTML) {
-    e.respondWith(
-      fetch(e.request).then(res => {
-        const clone = res.clone();
-        caches.open(CACHE).then(c => c.put(e.request, clone));
-        return res;
-      }).catch(() => caches.match(e.request))
-    );
-  } else {
-    e.respondWith(
-      caches.match(e.request).then(cached => cached || fetch(e.request))
-    );
-  }
+  e.respondWith(
+    caches.match(e.request).then(cached => cached || fetch(e.request))
+  );
 });
